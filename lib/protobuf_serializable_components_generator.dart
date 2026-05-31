a3cde6e4e3f31fe1069e8603c5aae0501bad5adb
vun <vunyunt@gmail.com>
2026-05-31 12:48:46 +0800

    Initial commit

---
+++ lib/protobuf_serializable_components_generator.dart
@@ -0,0 +1,7 @@
+library protobuf_serializable_components_generator;
+
+import 'package:build/build.dart';
+import 'src/core_component_generator.dart';
+
+Builder coreSerializableComponentsBuilder(BuilderOptions options) =>
+    CoreSerializableComponentsBuilder();
