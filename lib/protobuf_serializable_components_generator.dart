31188c66ad3d21ce5c784ebfada66d309464980c
vun <vunyunt@gmail.com>
2026-06-10 20:51:41 +0800

    feat: add builders for automatic protobuf registry and export indexing

--- lib/protobuf_serializable_components_generator.dart
+++ lib/protobuf_serializable_components_generator.dart
@@ -3,5 +3,9 @@ library protobuf_serializable_components_generator;
 import 'package:build/build.dart';
 import 'src/core_component_generator.dart';
 
+export 'src/descriptor_registry_builder.dart';
+export 'src/export_index_builder.dart';
+export 'src/type_registry_builder.dart';
+
 Builder coreSerializableComponentsBuilder(BuilderOptions options) =>
     CoreSerializableComponentsBuilder(options);
